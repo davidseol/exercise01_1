86a4f3649ada10c77bd74fe834e9701ec95430e9
david seol <hardroad07@gmail.com>
2022-05-31 17:43:49 +0900

    nothing

--- src/Exercise01_1.java
+++ src/Exercise01_1.java
@@ -1,5 +1,14 @@
 public class Exercise01_1 {
     public static void main(String[] args) {
-        System.out.println("hello. java!");
+        System.out.println("나는 문자열");
+        System.out.println('나');
+        System.out.println("안녕 나는 \"자바\" 야");
+        System.out.println("치킨은 살 안 쪄요. \n살은 내가 쪄요");
+        // 주석을 배워봅시다 라는 문장을 출력합니다.
+        System.out.println("주석을 배워봅시다.");
+        /* 이것은 여러 줄의 주석입니다.
+        두번쨰 줄 입니다.
+         */
+        System.out.println("위에는 여러줄의 주석이 있어요");
     }
 }
