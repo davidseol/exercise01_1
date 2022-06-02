ff038c35d04a6cbd82d109bca9022a013fdec943
david seol <hardroad07@gmail.com>
2022-06-02 17:24:00 +0900

    연산자

---
+++ src/Exercise01_3.java
@@ -0,0 +1,11 @@
+public class Exercise01_3 {
+    public static void main(String[] args) {
+        System.out.println(1 + 1.2);
+        System.out.println(7 / 2);
+        System.out.println(7 / 2.0);
+    }
+}
+
+// 정수와 실수의 연산 결과는 실수
+// 정수와 정수의 나눗셈 결과는 정수
+
