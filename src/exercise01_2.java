76f019946b21cda6b3e53ee1f6b783f70a6b3692
david seol <hardroad07@gmail.com>
2022-06-02 17:13:37 +0900

    변수

---
+++ src/exercise01_2.java
@@ -0,0 +1,10 @@
+public class exercise01_2 {
+    public static void main(String[] args) {
+        int ratio = 13;
+
+        System.out.println(100 * ratio / 100.0);
+        System.out.println(300 * ratio / 100.0);
+
+
+    }
+}
