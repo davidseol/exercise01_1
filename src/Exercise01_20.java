373949b985345b47b2b25443ed0215d7969ac02b
david seol <hardroad07@gmail.com>
2022-06-06 19:05:52 +0900

    featuring: 혼공자(연산자14)

---
+++ src/Exercise01_20.java
@@ -0,0 +1,10 @@
+
+// 다음 코드를 실행했을 때 출력 결과는 무엇입니까?
+
+public class Exercise01_20 {
+    public static void main(String[] args) {
+        int score = 85;
+        String result = (!(score>90)) ? "가" : "나";
+        System.out.println(result);
+    }
+}
