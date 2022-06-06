4660cf672aef4a04b856be9d5b9de202f89fa5ca
david seol <hardroad07@gmail.com>
2022-06-06 14:37:09 +0900

    featuring: 혼공자(연산자3)

---
+++ src/Exercise01_9.java
@@ -0,0 +1,14 @@
+
+
+/* 다음 코드에서 stop이 true일 떄 반복을 멈추고 싶습니다. 참고로 while(조건식) {}은 조건식이 true가 될 떄 중괄호
+{}안의 실행문을 반복해서 실행합니다.__________에 들어갈 알맞은 조건식을 작성해보세요.
+ */
+
+public class Exercise01_9 {
+    public static void main(String[] args) {
+        boolean stop = false;
+        while(!stop) {
+            stop = true;
+        }
+    }
+}
