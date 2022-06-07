5df49c55ef8fb62fc2dc61222ed2865486319914
david seol <hardroad07@gmail.com>
2022-06-07 17:20:26 +0900

    featuring: 혼공자(조건문8)

---
+++ src/Exercise02_8.java
@@ -0,0 +1,11 @@
+// for문의 1~100까지의 합 구하기
+
+public class Exercise02_8 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=1; i<=100; i++) {
+            sum = sum + i;
+        }
+        System.out.println("1~100의 합:" + sum);
+    }
+}
