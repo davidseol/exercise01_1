75a092c25d427d0191ee5a69666a34d9f1c4e3f3
david seol <hardroad07@gmail.com>
2022-06-16 17:26:15 +0900

    featuring: 혼공자(메소드5)

---
+++ src/chapter06_4/Calculator2Example.java
@@ -0,0 +1,13 @@
+package chapter06_4;
+
+public class Calculator2Example {
+    public static void main(String[] args) {
+        Calculator2 myCalc = new Calculator2();
+
+        double result1 = myCalc.areaRectangle(10);
+        double result2 = myCalc.areaRectangle(10,20);
+
+        System.out.println("정사각형 넓이=" + result1);
+        System.out.println("직사각형 넓이=" + result2);
+    }
+}
