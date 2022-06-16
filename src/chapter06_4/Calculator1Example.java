56bc0943dee1147d75a1970a1ded40b183a117e6
david seol <hardroad07@gmail.com>
2022-06-16 15:43:20 +0900

    featuring: 혼공자(메소드3)

---
+++ src/chapter06_4/Calculator1Example.java
@@ -0,0 +1,8 @@
+package chapter06_4;
+
+public class Calculator1Example {
+    public static void main(String[] args) {
+        Calculator1 myCalc = new Calculator1();
+        myCalc.execute();
+    }
+}
