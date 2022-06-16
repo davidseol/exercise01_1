5a9e572456688a0d62850f568c88b7f5438bacc5
david seol <hardroad07@gmail.com>
2022-06-16 18:37:36 +0900

    featuring: 혼공자(메소드6)

---
+++ src/chapter06_4/Car2Example.java
@@ -0,0 +1,11 @@
+package chapter06_4;
+
+public class Car2Example {
+    public static void main(String[] args) {
+        Car2 myCar = new Car2("포르쉐");
+        Car2 yourCar = new Car2("벤츠");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
