f579f3ab3fa53a325433c81c10a8348a64806729
david seol <hardroad07@gmail.com>
2022-06-17 15:30:34 +0900

    featuring: 혼공자(메소드7))

---
+++ src/chapter06_4/Car3.java
@@ -0,0 +1,15 @@
+package chapter06_4;
+
+public class Car3 {
+    int speed;
+
+    void run() {
+        System.out.println(speed + "으로 달립니다.");
+    }
+
+    public static void main(String[] args) {
+        Car3 myCar = new Car3();
+        myCar.speed = 60;
+        myCar.run();
+    }
+}
