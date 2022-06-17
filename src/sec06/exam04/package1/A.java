e6cdaddd21f1e226f87e31da1873e3e973010db0
david seol <hardroad07@gmail.com>
2022-06-17 20:28:05 +0900

    featuring: 혼공자(메소드12)

---
+++ src/sec06/exam04/package1/A.java
@@ -0,0 +1,10 @@
+package sec06.exam04.package1;
+
+public class A {
+    
+    //필드
+    
+    java a1 = new java(true);
+    java a2 = new java(1);
+   // java a3 = new java("문자열"); 같은 패키지안이라도 private 생성자 접근 불가
+}
---
+++ src/sec06/exam04/package1/java.java
@@ -0,0 +1,16 @@
+package sec06.exam04.package1;
+
+public class java {
+
+    // 필드
+    java a1 = new java(true);
+    java a2 = new java(1);
+    java a3 = new java("문자열");
+
+
+    // 생성자
+
+    public java(boolean b) {}
+    java(int b) {}
+    private java(String s) {}
+}
